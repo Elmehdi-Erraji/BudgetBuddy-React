87f3a491766bd13d59b31b6b40b6752f94ad716b
El Mehdi ER.RAJI <82772030+Elmehdi-Erraji@users.noreply.github.com>
2024-04-17 22:32:40 +0100

    quick fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-
+//here where all the components are rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
